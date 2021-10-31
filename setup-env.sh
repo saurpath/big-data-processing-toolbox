eca0463f5ca41bdf18be3f24b1dd6781033cddf3
Saurabh <spathak2@andrew.cmu.edu>
2021-10-31 19:43:14 -0400

    Enable container APIs

--- setup-env.sh
+++ setup-env.sh
@@ -1,4 +1,5 @@
 gcloud projects create saurpath-toolbox-demo --set-as-default
+gcloud services enable container.googleapis.com
 gcloud config set compute/zone us-central1-c 
 gcloud container clusters create toolbox --num-nodes=3
 gcloud container clusters get-credentials  --region=us-central1-c toolbox
