6849f7ce3b0e2cc8ed785d52ae255f7fb40056bd
Saurabh <spathak2@andrew.cmu.edu>
2021-10-31 20:07:47 -0400

    Fixed folders

--- deploy-everything.sh
+++ deploy-everything.sh
@@ -1,34 +1,34 @@
 # Jupyter Notebook
-kubctl apply -f jupyter/jupyter-deployment.yaml
-kubctl apply -f jupyter/jupyter-service.yaml
-kubctl apply -f jupyter/jupyter-lb.yaml
+kubectl apply -f jupyter/jupyter-deployment.yaml
+kubectl apply -f jupyter/jupyter-service.yaml
+kubectl apply -f jupyter/jupyter-lb.yaml
 
 # Hadoop namenode
-kubectl apply -f namenode-deployment.yaml
-kubectl apply -f namenode-service.yaml
-kubectl apply -f namenode-lb.yaml
+kubectl apply -f hadoop/namenode/namenode-deployment.yaml
+kubectl apply -f hadoop/namenode/namenode-service.yaml
+kubectl apply -f hadoop/namenode/namenode-lb.yaml
 
 sleep 10
 
 # Hadoop datanode
-kubectl apply -f datanode1-deployment.yaml
-kubectl apply -f datanode1-service.yaml
-kubectl apply -f datanode2-deployment.yaml
-kubectl apply -f datanode2-service.yaml
+kubectl apply -f hadoop/datanode/datanode1-deployment.yaml
+kubectl apply -f hadoop/datanode/datanode1-service.yaml
+kubectl apply -f hadoop/datanode/datanode2-deployment.yaml
+kubectl apply -f hadoop/datanode/datanode2-service.yaml
 
 # Sonarscanner
-kubectl apply -f sonarscanner-deployment.yaml
-kubectl apply -f sonarscanner-service.yaml
-kubectl apply -f sonarscanner-lb.yaml
+kubectl apply -f sonarscanner/sonarscanner-deployment.yaml
+kubectl apply -f sonarscanner/sonarscanner-service.yaml
+kubectl apply -f sonarscanner/sonarscanner-lb.yaml
 
 # Spark Master
-kubectl apply -f spark-master-deployment.yaml
-kubectl apply -f spark-master-service.yaml
-kubectl apply -f spark-master-ingress.yaml
+kubectl apply -f spark/spark-master-deployment.yaml
+kubectl apply -f spark/spark-master-service.yaml
+kubectl apply -f spark/spark-master-ingress.yaml
 
 sleep 10
 
 # Spark workers
-kubectl apply -f spark-worker1-deployment.yaml
-kubectl apply -f spark-worker2-deployment.yaml
+kubectl apply -f spark/spark-worker1-deployment.yaml
+kubectl apply -f spark/spark-worker2-deployment.yaml
 
