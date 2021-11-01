4d34272da30e6f4bfcb4ca16e03fe028d42972d9
Saurabh <spathak2@andrew.cmu.edu>
2021-10-31 20:01:31 -0400

    Added Spark

---
+++ deploy-everything.sh
@@ -0,0 +1,27 @@
+# Jupyter Notebook
+kubctl apply -f jupyter/jupyter-deployment.yaml
+kubctl apply -f jupyter/jupyter-service.yaml
+kubctl apply -f jupyter/jupyter-lb.yaml
+
+# Hadoop namenode
+kubectl apply -f namenode-deployment.yaml
+kubectl apply -f namenode-service.yaml
+kubectl apply -f namenode-lb.yaml
+
+sleep 10
+
+# Hadoop datanode
+kubectl apply -f datanode1-deployment.yaml
+kubectl apply -f datanode1-service.yaml
+kubectl apply -f datanode2-deployment.yaml
+kubectl apply -f datanode2-service.yaml
+
+# Sonarscanner
+kubectl apply -f sonarscanner-deployment.yaml
+kubectl apply -f sonarscanner-service.yaml
+kubectl apply -f sonarscanner-lb.yaml
+
+# Spark
+kubectl apply -f sonarscanner-deployment.yaml
+kubectl apply -f sonarscanner-service.yaml
+kubectl apply -f sonarscanner-lb.yaml
--- setup-env.sh
+++
@@ -1,5 +0,0 @@
-gcloud projects create saurpath-toolbox-demo --set-as-default
-gcloud services enable container.googleapis.com
-gcloud config set compute/zone us-central1-c 
-gcloud container clusters create toolbox --num-nodes=3
-gcloud container clusters get-credentials  --region=us-central1-c toolbox
